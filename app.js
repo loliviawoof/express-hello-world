fb4aafaf15cdfdc0d9745e8e73a8af15452cebdc
picis0503 <picis0503@gmail.com>
2022-12-20 19:05:11 -0300

    services

--- app.js
+++ app.js
@@ -2,58 +2,68 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const {Client} = require("pg")
+
+console.log("Conecting to " + process.env.PG)
+const pgClient = new Client(process.env.PG);
+pgClient.connect()
+  .then(() => console.log("Database connected"))
+  .catch(e => console.error("Error on database conection : " + e.message))
+
+app.use(express.json())
+
+app.get("/venues/find", async (req, res) => {
+  try{
+   const guest = req.query.guest
+   const indoorsoutdoors = req.query.indoorsoutdoors 
+
+   const queryResult = await pgClient.query(
+    "SELECT * FROM venues WHERE maxpeople > $1 and indoorsoutdoors = $2",
+     [parseInt(guest), indoorsoutdoors ]
+    )
+
+   res.json({reservations : queryResult.rows })
+  } catch (e) {
+    res.status(500).json({error: e.message});
+  }
+  }
+);
+
+
+
+app.post("/reservation", async (req, res) => {
+  try{
+    const body = req.body;
+    const queryResult = await pgClient.query(
+      "INSERT INTO reservations (guess, venueId, wantsCaterin, reservationDate, customerName, email, phone, active) VALUES ($1, $2, $3, $4, $5, $6, $7, true) RETURNING reservation_id;",
+      [body.guess, body.venueId, body.wantsCaterin, body.reservationDate, body.customerName, body.email, body.phone]
+    )
+    res.json({reservations : queryResult.rows })
+   } catch (e) {
+     res.status(500).json({error: e.message});
+   }
+});
+
+
+app.get("/reservation/:id", async (req, res) => {
+  try{
+    const queryResult = await pgClient.query("SELECT * FROM reservations WHERE reservation_id = $1", [req.params.id])
+    
+    res.json({reservations : queryResult.rows })
+  } catch (e) {
+    res.status(500).json({error: e.message});
+  }
+}
+);
+
+app.delete("/reservation/:id", async (req, res) => {
+  try{
+    const queryResult = await pgClient.query(
+      "UPDATE reservations SET active = false WHERE reservation_id = $1", [req.params.id])
+    res.json({reservations : queryResult.rows })
+   } catch (e) {
+     res.status(500).json({error: e.message});
+   }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello Leyla from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello Leyla from Render!
-    </section>
-  </body>
-</html>
-`
